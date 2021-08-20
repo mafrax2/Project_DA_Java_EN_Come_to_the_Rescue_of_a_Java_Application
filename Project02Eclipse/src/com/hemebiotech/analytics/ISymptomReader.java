12ff6c12980be2f763a45059d22421d2aea87694
mafrax2 <monsieurrobot2@gmail.com>
2021-08-20 16:16:35 +0200

    Squashed commit of the following:
    commit faa551d5499ed94afb426d53f702094cfa879246
    Author: mafrax2 <monsieurrobot2@gmail.com>
    Date:   Fri Aug 20 16:12:54 2021 +0200
    added getter/setter, added an exception on no filepath, added method
    with param filepath

--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
@@ -1,5 +1,6 @@
 package com.hemebiotech.analytics;
 
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -15,6 +16,16 @@ public interface ISymptomReader {
 	 * If no data is available, return an empty List
 	 * 
 	 * @return a raw listing of all Symptoms obtained from a data source, duplicates are possible/probable
+	 * @throws IOException 
 	 */
-	List<String> GetSymptoms ();
+	List<String> GetSymptoms () throws IOException;
+
+	/**
+	 * If no data is available, return an empty List
+	 * 
+	 * @param filepath to symptom file
+	 * @return a raw listing of all Symptoms obtained from a data source, duplicates are possible/probable
+	 * @throws IOException 
+	 */
+	List<String> GetSymptoms(String filepath) throws IOException;
 }
--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -13,24 +14,25 @@ import java.util.List;
 public class ReadSymptomDataFromFile implements ISymptomReader {
 
 	private String filepath;
-	
+
 	/**
 	 * 
-	 * @param filepath a full or partial path to file with symptom strings in it, one per line
+	 * @param filepath a full or partial path to file with symptom strings in it,
+	 *                 one per line
 	 */
-	public ReadSymptomDataFromFile (String filepath) {
+	public ReadSymptomDataFromFile(String filepath) {
 		this.filepath = filepath;
 	}
-	
+
 	@Override
-	public List<String> GetSymptoms() {
+	public List<String> GetSymptoms(String filepath) throws IOException {
 		ArrayList<String> result = new ArrayList<String>();
-		
+
 		if (filepath != null) {
 			try {
-				BufferedReader reader = new BufferedReader (new FileReader(filepath));
+				BufferedReader reader = new BufferedReader(new FileReader(filepath));
 				String line = reader.readLine();
-				
+
 				while (line != null) {
 					result.add(line);
 					line = reader.readLine();
@@ -39,9 +41,26 @@ public class ReadSymptomDataFromFile implements ISymptomReader {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+		} else {
+			throw new IOException("No filepath found, please give a Filepath");
 		}
-		
+		// ordering alphabetically
+		Collections.sort(result);
 		return result;
+
+	}
+
+	@Override
+	public List<String> GetSymptoms() throws IOException {
+		return this.GetSymptoms(filepath);
+	}
+
+	public String getFilepath() {
+		return filepath;
+	}
+
+	public void setFilepath(String filepath) {
+		this.filepath = filepath;
 	}
 
 }
