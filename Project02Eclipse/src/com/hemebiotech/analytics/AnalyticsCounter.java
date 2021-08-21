a1a7b943708cc4852d38680136b932ee9ac62771
mafrax2 <monsieurrobot2@gmail.com>
2021-08-21 15:43:24 +0200

    commit 7aed75dc9de83c0010f4e16b3cc0025a6adc9afa Author: mafrax2 <monsieurrobot2@gmail.com> Date:   Sat Aug 21 15:41:50 2021 +0200
    	added class Symptoms with method regroup and count

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,8 +1,8 @@
 package com.hemebiotech.analytics;
 
 import java.io.FileWriter;
-import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 public class AnalyticsCounter {
 	private static int headacheCount = 0; // initialize to 0
@@ -11,23 +11,12 @@ public class AnalyticsCounter {
 
 	public static void main(String args[]) throws Exception {
 
-		ReadSymptomDataFromFile symptomDataFromFile = new ReadSymptomDataFromFile("./Project02Eclipse/symptoms.txt");
+		SymptomDataReader symptomDataFromFile = new SymptomDataReader("./Project02Eclipse/symptoms.txt");
 
-		List<String> symptoms = symptomDataFromFile.GetSymptoms();
+		List<String> symptomsList = symptomDataFromFile.GetSymptoms();
 
-		int headCount = 0; // counts headaches
-		for (Iterator iterator = symptoms.iterator(); iterator.hasNext();) {
-			String symptom = (String) iterator.next();
-			if (symptom.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			} else if (symptom.equals("rush")) {
-				rashCount++;
-			} else if (symptom.contains("pupils")) {
-				pupilCount++;
-			}
-
-		}
+		Symptoms symptoms = new Symptoms(symptomsList);
+		Map<String, Long> symptomsMap = symptoms.regroupAndCount();
 
 		// next generate output
 		FileWriter writer = new FileWriter("result.out");
---
+++ Project02Eclipse/src/com/hemebiotech/analytics/IDataUtil.java
@@ -0,0 +1,21 @@
+package com.hemebiotech.analytics;
+
+import java.util.List;
+import java.util.Map;
+
+public interface IDataUtil {
+
+	/**
+	 * 
+	 * @param symptoms
+	 * @return map with key: symptom , value: number of occurrences
+	 */
+	Map<String, Long> regroupAndCount(List<String> symptoms);
+
+	/**
+	 * 
+	 * @return map with key: symptom , value: number of occurrences
+	 */
+	Map<String, Long> regroupAndCount();
+
+}
